4a78944a34c8513772fa36e40a8f0773f6282cee
Denis <decinpix@gmail.com>
2016-07-03 03:43:18 +0300

    modified func sum, templ func now

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <time.h>
 
-constexpr int sum(int lhs, int rhs, int count) noexcept{
-    auto s = 0;
+template<typename T, typename COUNT = size_t>
+constexpr decltype(auto) sum(T lhs, T rhs, COUNT count) noexcept{
+    T s = 0;
     for(auto i = 0; i < count ; ++i)
         s += lhs + rhs;
     return s;
 }
 
 int main(){
-    int tmp = 42;
     auto begin = clock();
-    const auto x = sum(tmp, 11, 1000000);
+    constexpr auto x = sum(42, 11, 1000000);
     begin = clock() - begin;
     std::cout << "Time: " << static_cast<double>(begin)/CLOCKS_PER_SEC*100 << std::endl;
     std::cout << "Value: " << x << std::endl;
